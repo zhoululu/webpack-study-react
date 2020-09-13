9e77bb6d01d82aec6abd35f91ddbe2148281c91a
zhoululu <1442802810@qq.com>
2020-09-13 15:35:33 +0800

    增加动态导入功能

--- src/search/index.js
+++ src/search/index.js
@@ -6,9 +6,27 @@ import './search.less'
 import logo from './images/logo.png'
 
 class Search extends React.Component {
+  constructor(prop) {
+    super(prop)
+    this.state = {
+      Text: null
+    }
+  }
+  loadComponent() {
+    import('./text.js').then((Text) => {
+      this.setState({
+        Text: Text.default
+      })
+    })
+  }
   render() {
+    const Text = this.state.Text
     return (
-      <div className="search">搜索文字的内容<img src={ logo } /></div>
+      <div className="search">
+        {
+          Text ? <Text /> : null 
+        }
+        搜索文字的内容<img src={ logo } onClick={ this.loadComponent.bind(this) } /></div>
     )
   }
 }
